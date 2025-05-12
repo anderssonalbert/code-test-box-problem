41458ada6f27d02d29a5f95ca5bf80ef495f63f9
Albert Andersson <albert.andersson@callistaenterprise.se>
2025-05-12 15:51:09 +0200

    refactor: adjusted thrown exception to illegalargument

--- src/main/java/se/callistaenterprise/boxproblem/domain/OrderRow.java
+++ src/main/java/se/callistaenterprise/boxproblem/domain/OrderRow.java
@@ -1,13 +1,17 @@
 package se.callistaenterprise.boxproblem.domain;
 
-import java.util.Objects;
-
 import static se.callistaenterprise.boxproblem.domain.Article.ARTICLES;
 
 public record OrderRow(int quantity, Article article) {
 
     public static OrderRow of(int quantity, int articleId) {
-        var article = Objects.requireNonNull(ARTICLES.get(articleId), "Article with ID=" + articleId + " does not exist");
+
+        var article = ARTICLES.get(articleId);
+        
+        if (article == null) {
+            throw new IllegalArgumentException("Article with ID=" + articleId + " does not exist");
+        }
+
         return new OrderRow(quantity, article);
     }
 
